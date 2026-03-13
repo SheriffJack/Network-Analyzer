2538532104d451fcb71541ec9449fda38cf0a09b
SheriffJack <monish.goel06@gmail.com>
2026-03-13 14:35:47 +0530

    server.py updated so that now it can support multiple clients.

--- server.py
+++ server.py
@@ -1,45 +1,61 @@
 import socket
+import threading
 
 HOST = "0.0.0.0"
 PORT = 5001
 FILE_NAME = "testfile.bin"
 BUFFER_SIZE = 1024
 
-# 1. Socket creation
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# 2. Binding socket to address and port
-server_socket.bind((HOST, PORT))
+def handle_client(conn, addr):
 
-# 3. Listening for incoming connections
-server_socket.listen(5)
-
-print("Server started and listening on port", PORT)
-
-while True:
-
-    # 4. Accept client connection
-    conn, addr = server_socket.accept()
-    print("Connection established with:", addr)
+    print("Client connected:", addr)
 
     try:
         with open(FILE_NAME, "rb") as f:
 
             while True:
+
                 data = f.read(BUFFER_SIZE)
 
                 if not data:
                     break
 
-                # 5. Send data to client
                 conn.sendall(data)
 
-        print("File sent successfully")
+        print("File sent to", addr)
+
+    except ConnectionResetError:
+        print("Client disconnected abruptly:", addr)
 
     except Exception as e:
         print("Error:", e)
 
     finally:
-        # 6. Close client connection
         conn.close()
-        print("Connection closed\n")
+        print("Connection closed:", addr)
+
+
+# socket creation
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# bind
+server_socket.bind((HOST, PORT))
+
+# listen
+server_socket.listen(5)
+
+print("Server running on port", PORT)
+
+while True:
+
+    # accept connection
+    conn, addr = server_socket.accept()
+
+    # create new thread
+    client_thread = threading.Thread(
+        target=handle_client,
+        args=(conn, addr)
+    )
+
+    client_thread.start()
